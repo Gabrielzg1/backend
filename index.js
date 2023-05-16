7456f419472e4f3e137bc43654fc82eed84986b0
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-15 23:16:16 -0300

    .

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 // URL da conexáo
 const mongoDBUrl = "mongodb://mongodb:27017/mydatabase";
+
 //Conectando o banco de dados
 mongoose
-  .connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Conexão bem-sucedida com o banco de dados MongoDB.");
-  })
-  .catch((error) => {
-    console.error("Erro ao conectar ao banco de dados MongoDB:", error);
-  });
+	.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Conexão bem-sucedida com o banco de dados MongoDB.");
+	})
+	.catch((error) => {
+		console.error("Erro ao conectar ao banco de dados MongoDB:", error);
+	});
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World - teste de atulizacao");
+	res.send("Hello World - teste de atulizacao");
 });
 
 //Rotas para os CRUDs
@@ -34,19 +35,19 @@ app.use("/admins", require("./routes/admins"));
 app.use("/users", require("./routes/users"));
 
 app.use((error, req, res, next) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  res.status(status).json({ message: message });
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	res.status(status).json({ message: message });
 });
 
 (async () => {
-  try {
-    await sequelize.sync({ force: false });
-    app.listen(3000);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		await sequelize.sync({ force: false });
+		app.listen(3000);
+	} catch (error) {
+		console.error(error);
+	}
 })();
 
 module.exports = app;
