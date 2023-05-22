c8914b81e360c3771f24f8c08b9e894d0838983e
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-22 17:53:53 -0300

    .

--- routes/admins.js
+++
@@ -1,8 +0,0 @@
-const controller = require("../controllers/admins");
-const router = require("express").Router();
-
-router.get("/", controller.index);
-router.get("/:adminId", controller.show);
-router.post("/", controller.create);
-
-module.exports = router;
