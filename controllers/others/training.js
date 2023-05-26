7c1afb4bb64d186fd27a76ca11e359b892be8913
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-26 19:13:00 -0300

    some rules

--- controllers/others/training.js
+++ controllers/others/training.js
@@ -1,74 +1,77 @@
 const Training = require("../../models/others/training");
+const User = require("../../models/permissions/user");
 
 class TrainingController {
-	async index(req, res) {
-		try {
-			const tranings = await Training.find();
-			return res.json(tranings).status(200);
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: "Internal server error." });
-		}
-	}
+  async index(req, res) {
+    try {
+      const tranings = await Training.find();
+      return res.json(tranings).status(200);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
+    }
+  }
 
-	async show(req, res) {
-		try {
-			const { id } = req.params;
-			const traning = await Training.findById(id);
-			if (!traning) return res.status(404).json({ msg: "Admin not found" });
-			return res.json(traning);
-		} catch (err) {
-			console.log(err);
-			return res.status(500).json({ error: "Internal server error." });
-		}
-	}
-	async create(req, res) {
-		try {
-			const {
-				name,
-				initialInscriptionDate,
-				finalInscriptionDate,
-				initialTraningDate,
-				finalTraningDate,
-				minimumAmount,
-				maximumAmount,
-			} = req.body;
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const traning = await Training.findById(id);
+      if (!traning) return res.status(404).json({ msg: "Admin not found" });
+      return res.json(traning);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Internal server error." });
+    }
+  }
+  async create(req, res) {
+    try {
+      const {
+        name,
+        initialInscriptionDate,
+        finalInscriptionDate,
+        initialTrainingDate,
+        finalTrainingDate,
+        minimumAmount,
+        maximumAmount,
+      } = req.body;
 
-			const newTraning = await Training.create({
-				name,
-				initialInscriptionDate,
-				finalInscriptionDate,
-				initialTraningDate,
-				finalTraningDate,
-				minimumAmount,
-				maximumAmount,
-			});
+      const newTraning = await Training.create({
+        name,
+        initialInscriptionDate,
+        finalInscriptionDate,
+        initialTrainingDate,
+        finalTrainingDate,
+        minimumAmount,
+        maximumAmount,
+      });
 
-			return res.status(201).json(newTraning);
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: "Internal server error." });
-		}
-	}
+      return res.status(201).json(newTraning);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
+    }
+  }
 
-	async updateStudents(req, res) {
-		try {
-			const { userId } = req.body;
-			const { traningId } = req.params;
-			const training = await Training.findById(traningId);
-			if (!training) return res.status(422).json();
+  async updateStudents(req, res) {
+    try {
+      const { userId } = req.body;
+      const { traningId } = req.params;
+      const training = await Training.findById(traningId);
+      if (!training) return res.status(422).json();
+      const user = await User.findById(userId);
+      if (!user) return res.status(422).json({ msg: "Usuário não encontrado" });
 
-			if (training.students.indexOf(userId) !== -1)
-				return res.status(422).json();
+      if (training.students.indexOf(userId) !== -1)
+        return res.status(422).json({ msg: "Usuário já cadastrado" });
 
-			let newStudents = [].concat(training.students, userId);
+      let newStudents = [].concat(training.students, userId);
 
-			await training.updateOne({ students: newStudents });
-			return res.status(201).json(newStudents);
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: "Internal server error." });
-		}
-	}
+      await training.updateOne({ students: newStudents });
+      return res.status(201).json();
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
+    }
+  }
 }
 module.exports = new TrainingController();
