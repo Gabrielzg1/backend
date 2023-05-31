cb77f6ffe007278736598919c71c1e3089c835d7
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-31 18:03:57 -0300

    bugfix

--- routes/permissions/users.js
+++ routes/permissions/users.js
@@ -2,7 +2,7 @@ const controller = require("../../controllers/permissions/users");
 const router = require("express").Router();
 
 router.get("/", controller.index);
-router.get("/:userId", controller.show);
+router.get("/:id", controller.show);
 router.post("/", controller.create);
 router.put("/activity/applied/:userId", controller.updateAppliedActivity);
 router.put("/activity/finished/:userId", controller.updateFinishedActivity);
